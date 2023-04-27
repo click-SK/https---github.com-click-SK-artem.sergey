b58f5d005008aa97014bf136267f02a703bd1f9d
click-SK <sashakushnir4991@gmail.com>
2023-04-27 17:10:40 +0300

    pdf file chage

--- src/components/PdfFile.jsx
+++ src/components/PdfFile.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Font, View, Page, Image, Text, Document, StyleSheet } from '@react-pdf/renderer';
+import {Font, View, Page, Image, Text, Document, StyleSheet, Link } from '@react-pdf/renderer';
 import Logo from '../img/logo.png';
+import { faAlignJustify } from '@fortawesome/free-solid-svg-icons';
 // import Roboto from '../fonts/roboto/Roboto-Regular.ttf'
 
 // Font.registerHyphenationCallback(word => [word]);
@@ -24,12 +25,19 @@ const styles = StyleSheet.create({
     tittle:{
         fontSize: 24,
         textAlign: 'center',
-
+        marginTop:'20px'
     },
     text:{
         margin: 12,
         fontSize: 14,
-        textAlign: "justify",
+        textAlign: 'center',
+        flex: '1'
+    },
+    Hedertext:{
+        margin: 12,
+        fontSize: 14,
+        textAlign: 'center',
+        flex: 1
     },
     image:{
         marginVertical: 15,
@@ -37,17 +45,40 @@ const styles = StyleSheet.create({
     },
     header:{
         fontSize: 12,
-        // marginBottom: 20,
-        textAlign: "center",
+        textAlign: 'right',
         color: "grey"
     },
     section: {
+        marginBottom: '2px',
         flexDirection: 'row',
-        margin: 10,
-        padding: 10,
-        // flexGrow: 1,
-      }
+        borderBottomWidth: 1,
+        borderBottomColor: '#eee',
+        display: 'flex',
+        alignContent: 'stretch',
+        alignItems: 'center',
+        width:'100%'
+      },
+    tableHeder:{
+        marginTop: '10px',
+        marginBottom: '5px',
+        flexDirection: 'row',
+        borderBottomWidth: 1,
+        borderBottomColor: '#eee',
+        display: 'flex',
+        alignContent: 'stretch',
+        alignItems: 'center',
+        width:'100%'
+    },
+
 });
+
+{/* <View style={styles.section}>
+{arr.map((item, idx)=>(
+    <Text style={styles.text} key={idx}>
+    {item}
+    </Text>
+))}
+</View> */}
 
 const PdfFile = () => {
     const arr = [1,2,3,4,5]
@@ -57,8 +88,6 @@ const PdfFile = () => {
                 <Image style={styles.image} src={Logo}/>
                 <Text style={styles.header}>
                 +38 (067) 320 60 23
-                skloexpert.ua@gmail.com
-                Адреса нашого офісу: 46000, м. Тернопіль, вул. Текстильна, 42 ПН-ПТ: 09:00-18:00
                 </Text>
                 <Text style={styles.header}>
                 skloexpert.ua@gmail.com
@@ -66,33 +95,112 @@ const PdfFile = () => {
                 <Text style={styles.header}>
                 Адреса нашого офісу: 46000, м. Тернопіль, вул. Текстильна, 42 ПН-ПТ: 09:00-18:00
                 </Text>
-                <View style={styles.section}>
-                    <Text style={styles.text}>
+                <Link style={styles.header} src="http://skloexpert.com.ua/">
+                http:/skloexpert.com.ua/
+                </Link>
+                <Text style={styles.tittle}>
+                    Замовлення
+                </Text>
+                <View style={styles.tableHeder}>
+                    <Text style={styles.Hedertext}>
                         Назва
                     </Text>
-                    <Text style={styles.text}>
-                        Комплектація
-                    </Text>
-                    <Text style={styles.text}>
+                    <Text style={styles.Hedertext}>
                         Кількість
                     </Text>
-                    <Text style={styles.text}>
+                    <Text style={styles.Hedertext}>
                         Ціна
                     </Text>
                 </View>
                 <View style={styles.section}>
-                    {arr.map((item)=>(
-                        <Text style={styles.text}>
-                        {item}
-                        </Text>
-                    ))}
+                    <Text style={styles.text}>
+                        Дзеркало "Стандарт" з фоновою підсвіткою
+                        1х1м
+                    </Text>
+                    <Text style={styles.text}>
+                        1 шт
+                    </Text>
+                    <Text style={styles.text}>
+                        4200 грн
+                    </Text>
                 </View>
-                <Text style={styles.text}>
-
-                </Text>
-                <Text style={styles.text}>
-                    Замовлення
-                </Text>
+                <View style={styles.section}>
+                    <Text style={styles.text}>
+                        Металева рамка
+                    </Text>
+                    <Text style={styles.text}>
+                        4 м
+                    </Text>
+                    <Text style={styles.text}>
+                        1200 грн
+                    </Text>
+                </View>
+                <View style={styles.section}>
+                    <Text style={styles.text}>
+                        Фронтальна підсвітка
+                    </Text>
+                    <Text style={styles.text}>
+                        
+                    </Text>
+                    <Text style={styles.text}>
+                        1200 грн
+                    </Text>
+                </View>
+                <View style={styles.section}>
+                    <Text style={styles.text}>
+                        Сенсорний вимикач
+                    </Text>
+                    <Text style={styles.text}>
+                        1шт
+                    </Text>
+                    <Text style={styles.text}>
+                        550 грн
+                    </Text>
+                </View>
+                <View style={styles.section}>
+                    <Text style={styles.text}>
+                        Кабель підключення + вилка
+                    </Text>
+                    <Text style={styles.text}>
+                        3м
+                    </Text>
+                    <Text style={styles.text}>
+                        130 грн
+                    </Text>
+                </View>
+                <View style={styles.section}>
+                    <Text style={styles.text}>
+                        Підігрів
+                    </Text>
+                    <Text style={styles.text}>
+                        Так
+                    </Text>
+                    <Text style={styles.text}>
+                        1000 грн
+                    </Text>
+                </View>
+                <View style={styles.section}>
+                    <Text style={styles.text}>
+                        Покраска 
+                    </Text>
+                    <Text style={styles.text}>
+                        Ні
+                    </Text>
+                    <Text style={styles.text}>
+                        
+                    </Text>
+                </View>
+                <View style={styles.section}>
+                    <Text style={styles.text}>
+                        
+                    </Text>
+                    <Text style={styles.text}>
+                        Заальна вартість
+                    </Text>
+                    <Text style={styles.text}>
+                        8280 грн
+                    </Text>
+                </View>
             </Page>
         </Document>
     );
