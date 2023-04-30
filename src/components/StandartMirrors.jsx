bc2459dbf2272187ead431e33c25c2dc0b4c0a3f
click-SK <sashakushnir4991@gmail.com>
2023-04-30 18:42:10 +0300

    over city 5 marginTop

--- src/components/StandartMirrors.jsx
+++ src/components/StandartMirrors.jsx
@@ -427,6 +427,7 @@ const StandartMirrors = ({ data }) => {
                       <div className="checkbox_wrap ">
                         <input id="checkbox5"  className="checkbox" type='checkbox' checked={delivery} onChange={isDelivery}/>
                         <label className="checkbox-label" htmlFor="checkbox5"></label>
+                        <p style={{marginTop: 5}}>За місто</p> 
                       </div>
                       <input className="cabel width_delivery" type="number" placeholder="Відстань - км" value={deliveryRoadDistance} onChange={(e) => roadDistance(e)}/>
                   </div>
