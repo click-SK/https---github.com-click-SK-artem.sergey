cd9e61711b6ff2f3a4c194e7eca15aee678abf5a
click-SK <sashakushnir4991@gmail.com>
2023-04-30 18:33:08 +0300

    add func input mirrors delivery

--- src/components/StandartMirrors.jsx
+++ src/components/StandartMirrors.jsx
@@ -25,6 +25,17 @@ const StandartMirrors = ({ data }) => {
   const [totalSum, setTotalSum] = useState(0);
   const [finishMirrorPdf, setFinishMirrorPdf] = useState({});
   const [validationInput, setValidationInput] = useState(false);
+  const [isAssemblingt, setIsAssembling] = useState(false);
+  const [minInstallation, setMinInstallation] = useState(false);
+  const [adress, setAdress] = useState('');
+  const [deliveryRoadDistance, setDeliveryRoadDistance] = useState('');
+  const [delivery, setDelivery] = useState(false);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [surname, setSurname] = useState('');
+  const [numberPhone, setNumberPhone] = useState('');
+  const [orderComent, setOrderComent] = useState('');
+  // const [intslPrice, setIntslPrice] = useState(0);
   // const keyCsv = [
   //   {"Форма скла": currentType?.name },
   //   {"Тип дзеркала" : 'З фоновою підсвідкою'}
@@ -53,6 +64,31 @@ const StandartMirrors = ({ data }) => {
       const resSizePrice = calcSquareMeter * currentGoods?.price;
       const resCordSum = currentCord * priceMeterCord;
       const resFrameSum = sizeFrame * (currentFrame?.price || 0);
+      let intslPrice = 0;
+      let deliveryPrice = 0;
+      let deliveryPriceOverSity = 0;
+      let deliveryFinalyPrice = 0;
+
+        if (calcSquareMeter < 2){
+           intslPrice = calcSquareMeter * 300
+        } else if (calcSquareMeter > 2){
+          intslPrice = calcSquareMeter * 350
+        };
+        
+        if (adress != ''){
+          deliveryPrice = 200
+        }
+
+        if (delivery){
+          deliveryPriceOverSity = Number(deliveryRoadDistance) * 26
+        }
+
+        
+
+        console.log( "доставка over sity", deliveryRoadDistance);
+        console.log( "adress", adress);
+        console.log( "price", deliveryPrice);
+
 
       console.log('priceMeterCord',priceMeterCord);
       console.log('calcSize',calcSize);
@@ -62,7 +98,7 @@ const StandartMirrors = ({ data }) => {
       console.log('resFrameSum',resFrameSum);
 
   
-      const total = (resSizePrice || 0) + (resCordSum || 0) + (resFrameSum || 0) + (currentSwitch?.price || 0) + (isPainting ? currentColor?.price || 600 : 0) + (isWarmedUp ? warmedUpPrice : 0);
+      const total = (resSizePrice || 0) + (resCordSum || 0) + (resFrameSum || 0) + (currentSwitch?.price || 0) + (isPainting ? currentColor?.price || 600 : 0) + (isWarmedUp ? warmedUpPrice : 0) + (minInstallation ? 500 : 0) + (isAssemblingt ? intslPrice : 0) + (delivery ? deliveryPriceOverSity : deliveryPrice) ;
   
       const finishedMirros = {
         type: currentType?.name,
@@ -75,13 +111,21 @@ const StandartMirrors = ({ data }) => {
         frameName: currentFrame?.name,
         switchName: currentSwitch?.name,
         switchPrice: currentSwitch?.price,
+        backLight:currentBackLight,
         cord: currentCord,
         warmerUp: isWarmedUp ? 'Так' : 'Ні', 
         painting: isPainting ? 'Так' : 'Ні',
         colorName: isPainting ? currentColor?.name : '-',
         colorPrice: isPainting ? currentColor?.price : '-',
-        total: total,
-        print: "hello"
+        adress:adress,
+        deliveryPriceOverSity: deliveryPriceOverSity,
+        deliveryPriceOver: deliveryPriceOverSity,
+        firstName: firstName,
+        lastName: lastName,
+        surname: surname,
+        numberPhone: numberPhone,
+        orderComent: orderComent,
+        total: total
       }
   
       setFinishMirrorPdf(finishedMirros)
@@ -91,7 +135,11 @@ const StandartMirrors = ({ data }) => {
     } else {
       setValidationInput(true)
     }
+
+
   }
+
+
 
 
   const selectTypeFunc = (e) => {
@@ -134,11 +182,56 @@ const StandartMirrors = ({ data }) => {
     const paintingObj = data?.option?.painting;
     setIsPainting(isPainting => !isPainting)
   }
-
+  
   const selectedColorFunc = (e) => {
     const selectedColor = JSON.parse(e.target.value);
     setCurrentColor(selectedColor);
   }
+  
+  const changeIsAssemblingt = () => {
+    // const paintingObj = data?.option?.painting;
+    setIsAssembling(isAssemblingt => !isAssemblingt)
+  }
+
+    const changeMinInstallationFunc = () => {
+    // const paintingObj = data?.option?.painting;
+    setMinInstallation(minInstallation => !minInstallation)
+  }
+    const isDelivery = () => {
+    // const paintingObj = data?.option?.painting;
+    setDelivery(delivery => !delivery)
+  }
+
+  const addAdress = (e) => {
+    // const cordObj = data?.option?.cord;
+    setAdress(e.target.value);
+  }
+  const roadDistance = (e) => {
+    // const cordObj = data?.option?.cord;
+    setDeliveryRoadDistance(e.target.value);
+  }
+  const addFirstName = (e) => {
+    // const cordObj = data?.option?.cord;
+    setFirstName(e.target.value);
+  }
+  const addLastName = (e) => {
+    // const cordObj = data?.option?.cord;
+    setLastName(e.target.value);
+  }
+  const addSurname = (e) => {
+    // const cordObj = data?.option?.cord;
+    setSurname(e.target.value);
+  }
+  const addPhone = (e) => {
+    // const cordObj = data?.option?.cord;
+    setNumberPhone(e.target.value);
+  }
+  const addComent = (e) => {
+    // const cordObj = data?.option?.cord;
+    setOrderComent(e.target.value);
+  }
+
+ 
 
   // console.log('currentType',currentType);
   // console.log('currentGoods',currentGoods);
@@ -151,7 +244,7 @@ const StandartMirrors = ({ data }) => {
   // console.log('isWarmedUp',isWarmedUp);
   // console.log('isPainting',isPainting);
   // console.log('currentColor',currentColor);
-  console.log('totalSum',totalSum);
+
 
   return (
     <div className="wrap_item mirrors_item">
@@ -194,13 +287,13 @@ const StandartMirrors = ({ data }) => {
 
             <div className="choose_item item_mirrors">
             <h3>Розмір (см)</h3>
-            <div class=" input_miroor">
+            <div className=" input_miroor">
               <div>
-              <input class=" input_miroor_item cabel" type="number" placeholder="Ширина" value={sizeWidthMirrors} onChange={(e) => setSizeWidthMirrors(e.target.value)}/>
+              <input className=" input_miroor_item cabel" type="number" placeholder="Ширина" value={sizeWidthMirrors} onChange={(e) => setSizeWidthMirrors(e.target.value)}/>
               <p style={{color: 'red'}}>{validationInput && currentGoods && currentType && 'Введіть данні'}</p>
               </div>
               <div>
-              <input class="input_miroor_item  cabel" type="number" placeholder="Висота" value={sizeHeightMirrors} onChange={(e) => setSizeHeightMirrors(e.target.value)}/>
+              <input className="input_miroor_item  cabel" type="number" placeholder="Висота" value={sizeHeightMirrors} onChange={(e) => setSizeHeightMirrors(e.target.value)}/>
               <p style={{color: 'red'}}>{validationInput && currentGoods && currentType && 'Введіть данні'}</p>
               </div>
             </div>
@@ -228,8 +321,8 @@ const StandartMirrors = ({ data }) => {
 
         <div className="choose_item item_mirrors">
             <h3>Розмір рамки (м)</h3>
-            <div class="input_miroor">
-              <input class="input_miroor_frame cabel" type="number" value={sizeFrame} onChange={(e) => setSizeFrame(e.target.value)}/>
+            <div className="input_miroor">
+              <input className="input_miroor_frame cabel" type="number" value={sizeFrame} onChange={(e) => setSizeFrame(e.target.value)}/>
             </div>
             </div>
       
@@ -312,27 +405,50 @@ const StandartMirrors = ({ data }) => {
         </select>
         </div>
 
-        <div className="choose_item item_mirrors">
-      <h3>Монтаж:</h3>
-        <input className="cabel" placeholder="Довжина кабелю" value={currentCord} onChange={(e) => changeCord(e)}/>
+        <div className="choose_item item_mirrors item_montaje">
+        <h3>Монтаж:</h3>
+        <div className="montaje_wrap">
+          <div className="checkbox_wrap montaje">
+            <input id="checkbox3"  className="checkbox" type='checkbox' checked={isAssemblingt} onChange={changeIsAssemblingt}/>
+            <label className="checkbox-label" htmlFor="checkbox3"></label>
+          </div>
+          <div className="checkbox_wrap montaje">
+            <input id="checkbox4"  className="checkbox" type='checkbox' checked={minInstallation} onChange={changeMinInstallationFunc}/>
+            <label className="checkbox-label checkbox-label4" htmlFor="checkbox4"></label>
+            <p>Мінімальний монтаж - 500грн</p> 
+          </div>
+        </div>
       </div>
-      <div className="choose_item item_mirrors">
+      <div className="choose_item item_mirrors item_delivery">
       <h3>Доставка</h3>
-        <input className="cabel" placeholder="Довжина кабелю" value={currentCord} onChange={(e) => changeCord(e)}/>
+              <div className="delivery_wrap">
+                  <input className="cabel" placeholder="Адреса доставки" value={adress} onChange={(e) => addAdress(e)}/>
+                  <div className="delivery_addres">
+                      <div className="checkbox_wrap ">
+                        <input id="checkbox5"  className="checkbox" type='checkbox' checked={delivery} onChange={isDelivery}/>
+                        <label className="checkbox-label" htmlFor="checkbox5"></label>
+                      </div>
+                      <input className="cabel width_delivery" type="number" placeholder="Відстань - км" value={deliveryRoadDistance} onChange={(e) => roadDistance(e)}/>
+                  </div>
+              </div>
       </div>
-      <div className="choose_item item_mirrors">
+      <div className="choose_item item_mirrors item_fullname">
       <h3>ПІБ:</h3>
-        <input className="cabel" placeholder="Довжина кабелю" value={currentCord} onChange={(e) => changeCord(e)}/>
-        <input className="cabel" placeholder="Довжина кабелю" value={currentCord} onChange={(e) => changeCord(e)}/>
-        <input className="cabel" placeholder="Довжина кабелю" value={currentCord} onChange={(e) => changeCord(e)}/>
+        <div className="fullname_wrap">
+          <div className="name_lastname">
+            <input className="cabel" placeholder="Ім'я" value={firstName} onChange={(e) => addSurname(e)} />
+            <input className="cabel" placeholder="Прізвище" value={lastName} onChange={(e) => addFirstName(e)}/>
+          </div>
+          <input className="cabel" placeholder="По батькові" value={surname} onChange={(e) => addLastName(e)}/>
+        </div>
       </div>
       <div className="choose_item item_mirrors">
       <h3>Телефон</h3>
-        <input className="cabel" placeholder="Довжина кабелю" value={currentCord} onChange={(e) => changeCord(e)}/>
+        <input className="cabel" placeholder="+ 38 (0ХХ) ХХХ ХХ ХХ " value={numberPhone} onChange={(e) => addPhone(e)}/>
       </div>
-      <div className="choose_item item_mirrors">
+      <div className="choose_item item_mirrors item_textarea">
       <h3>Деталі замовлення</h3>
-        <textarea className="cabel" style={{width: "70%", height:100}} name="" id="" cols="30" rows="10"></textarea>
+        <textarea className="cabel" style={{width: "70%", height:"100%"}} value={orderComent} name="" id="" cols="30" rows="10" onChange={(e) => addComent(e)}></textarea>
       </div>
 
         <div className="footer_calc">
