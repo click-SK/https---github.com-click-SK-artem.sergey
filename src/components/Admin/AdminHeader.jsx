7452a2e2724da06fcfce7d886dadebefc10c1e76
click-SK <sashakushnir4991@gmail.com>
2023-05-07 11:50:41 +0300

    add style

--- src/components/Admin/AdminHeader.jsx
+++ src/components/Admin/AdminHeader.jsx
@@ -5,12 +5,12 @@ const AdminHeader = () => {
     return (
         <div className='admin_rout'>
             <div>
-                <a href='/admin-mirrors'>Mirrors</a>
-                <a href='/admin-cosmetic-mirrors'>Cosmetic mirrors</a>
-                <a href='/admin-showers'>Shower</a>
-                <a href='/admin-showers-client'>Shower Client</a>
-                <a href='/admin-dashki'>Dashki</a>
-                <a href='/admin-glass-partition'>Glass Partition</a>
+                <a href='/admin-showers'>Душові кабіни</a>
+                <a href='/admin-mirrors'>Дзеркала</a>
+                <a href='/admin-cosmetic-mirrors'>Косметичні дзеркала</a>
+                <a href='/admin-showers-client'>Душ кабіни для клієнта</a>
+                <a href='/admin-dashki'>Дашки</a>
+                <a href='/admin-glass-partition'>Скляні перегородки</a>
             </div>
         </div>
     );
