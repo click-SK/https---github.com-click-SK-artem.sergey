a6f5f6caba57e785c799d043985b35b359218cff
click-SK <sashakushnir4991@gmail.com>
2023-05-10 19:44:44 +0300

    partishion pdf

--- src/components/PdfFile/PdfFilePartitionManager.jsx
+++ src/components/PdfFile/PdfFilePartitionManager.jsx
@@ -224,8 +224,8 @@ const PdfShowerManadger = ({order, cart}) => {
         const fileFinish = {
             glass:{
                 price: revGlassPrice,
-                name: glass,
-                size: glassColorName,
+                name: "Скло",
+                size: glass,
             },
             currentProcessingStandart:{
                 price: currentProcessingStandartPrice,
